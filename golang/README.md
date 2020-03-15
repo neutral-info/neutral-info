c78829549daa251593b62dea21c445b2cd7d06c7
bangyuwen <bangyuwen@gmail.com>
2020-03-15 21:39:41 +0800

    Update Data Structures

--- golang/README.md
+++ golang/README.md
@@ -1,4 +1,12 @@
-# docs
+# Dcard Crawler
+
+## Docs
 
 golang version scrapy: [colly](http://go-colly.org/)
 dcard python version crawler: [dcard-spider](https://github.com/leVirve/dcard-spider)
+dcard API tutorial: [【爬蟲】從 Dcard 網站看爬蟲入門(III)](https://medium.com/爬蟲-從dcard網站看爬蟲入門-iii-ded52759d922)
+dcard API tutorial: [#99 串接 Dcard API，模仿開發 Dcard App](https://medium.com/彼得潘的試煉-勇者的-100-道-swift-ios-app-謎題/99-串接-dcard-api-模仿-dcard-app-969dbfe83dc6)
+
+## API List
+
+[Latest Posts](https://www.dcard.tw/_api/forums)
--- golang/dcard/main.go
+++ golang/dcard/main.go
@@ -9,12 +9,12 @@ import (
 	"github.com/gocolly/colly"
 )
 
-// Forum is the forum description forum Dcard
+// Forum is the forum of Dcard
 type Forum struct {
 	Alias             string `json:"alias"`             // "midnightlab"
 	Name              string `json:"name"`              // "午夜實驗室"
 	Description       string `json:"description"`       // "午夜實驗室10/6、10/7即將在華山登場！這裏提供大家交流活動資訊與討論，請大家要遵守 Dcard 板規喔！"
-	SubscriptionCount string `json:"subscriptionCount"` // 1842
+	SubscriptionCount int    `json:"subscriptionCount"` // 1842
 	CreatedAt         string `json:"createdAt"`         //"2016-05-14T19:15:15.698Z"
 	UpdatedAt         string `json:"updatedAt"`         //"2018-11-05T03:24:32.914Z"
 	Invisible         bool   `json:"invisible"`
@@ -32,6 +32,54 @@ type Forum struct {
 	PostCount            struct {
 		Last30Days int `json:"last30Days"`
 	} `json:"postCount"`
+}
+
+// Post is the post data structure of Dcard
+type Post struct {
+	ID                string `json:"id"`                // "63a0d93e-acc5-4664-b55a-81e6fe0a4d88",
+	Alias             string `json:"alias"`             // "csu",
+	Name              string `json:"name"`              // "正修科大",
+	Description       string `json:"description"`       // "正修科大板，一個能讓你暢所欲言的地方。在這裡，卡友們可以盡情討論校園裡的大小事，舉凡課程資訊、教授教學評價，又或是學校活動，只要是你想要知道的資訊，都能在校板中迅速獲得解答！",
+	SubscriptionCount int    `json:"subscriptionCount"` // 5256
+	Subscribed        bool   `json:"subscribed"`
+	Read              bool   `json:"read"`
+	CreatedAt         string `json:"createdAt"` // "2016-06-29T02:36:10.703Z"
+	UpdatedAt         string `json:"updatedAt"` // "2019-09-26T10:24:23.553Z"
+	CanPost           bool   `json:"canPost"`
+	IgnorePost        bool   `json:"ignorePost"`
+	Invisible         bool   `json:"invisible"`
+	IsSchool          bool   `json:"isSchool"`
+	FullyAnonymous    bool   `json:"fullyAnonymous"`
+	CanUseNickname    bool   `json:"canUseNickname"`
+	PostThumbnail     struct {
+		Size string `json:"size"` // "small"
+	} `json:"postThumbnail"`
+	ShouldCategorized    bool     `json:"shouldCategorized"`
+	TitlePlaceholder     string   `json:"titlePlaceholder"`     // ""
+	PostTitlePlaceholder string   `json:"postTitlePlaceholder"` // ""
+	IPCountryCondition   struct{} `json:"ipCountryCondition"`
+	Subcategories        []string `json:"subcategories"` // ["課程評價"]
+	Topics               []string `json:"topics"`
+	Nsfw                 bool     `json:"nsfw"`
+	MediaThreshold       struct{} `json:"mediaThreshold"`
+	LimitCountries       int      `json:"limitStage"`
+	AvailableLayouts     []string `json:"availableLayouts"` // ["classic"]
+	HeroImage            struct {
+		URL    string `json:"url"`    // "https://megapx-assets.dcard.tw/images/26ca693f-e13f-4342-9dc4-ff0de3dd32d5/orig.jpeg"
+		Type   string `json:"type"`   // "image/jpeg"
+		Width  int    `json:"width"`  // 1800
+		Height int    `json:"height"` // 600
+	} `json:"heroImage"`
+	Logo struct {
+		URL    string `json:"url"`    // "https://megapx-assets.dcard.tw/images/0cc0d5ae-535f-4e65-bddb-aa4aed767f05/orig.jpeg",
+		Type   string `json:"type"`   // "image/jpeg"
+		Width  int    `json:"width"`  // 200
+		Height int    `json:"height"` // 200
+	} `json:"logo"`
+	PostCount struct {
+		Last30Days int `json:"last30Days"` // 168
+	} `json:"postCount"`
+	Favorite bool `json:"favorite"`
 }
 
 func main() {
