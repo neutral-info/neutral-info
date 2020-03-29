8a1eb849818c3ee3f682b3068da84618c07694fa
bangyuwen <bangyuwen@gmail.com>
2020-03-30 00:01:04 +0800

    schedule from ptt

--- python/ptt/__main__.py
+++ python/ptt/__main__.py
@@ -1,10 +1,36 @@
 
+import time
+from datetime import datetime
+
+import click
+import schedule
+
 from crawler import PttWebCrawler
 
 
-def main():
+def heart_beat():
+    print(f"Alive at {datetime.now()}")
+
+
+def run():
     crawler = PttWebCrawler(as_lib=True)
-    crawler.parse_articles(100, 101, "PublicServan")
+    last_page = crawler.getLastPage('Gossiping')
+
+    crawler.parse_articles(last_page, last_page, "Gossiping")  # crawl single page for testing
+    # crawler.parse_articles(last_page - 150, last_page, "Gossiping")
+
+
+@click.command()
+@click.option('-s', '--schedule_arg', is_flag=True, help="run every 2 hours")
+def main(schedule_arg):
+    if schedule_arg is True:
+        schedule.every(2).hours.do(run)
+        schedule.every().minutes.do(heart_beat)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    else:
+        run()
 
 
 if __name__ == "__main__":
