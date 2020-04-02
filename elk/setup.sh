32d0752a66258f3629b89c2d9dccaf9858dfd01e
bangyuwen <bangyuwen@gmail.com>
2020-04-02 20:06:54 +0800

    elk runnable init

---
+++ elk/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# related doc: https://elk-docker.readthedocs.io/
+
+# change setting for elastic search
+# run this or add to /etc/sysctl.conf
+sysctl -w vm.max_map_count=262144
+
+docker pull sebp/elk
+docker-compose up
