7c78195f697a4865f6424e1b4ae1ceece4fd2af8
kiko555 <kiko555@gmail.com>
2020-12-24 01:29:13 +0800

    因應Docker階段自動建立初始資料的動作，一直無法正常，自動匯入的中文資料是亂碼，因此先略過，改以後續手動匯入。

--- sqls/1init.sql
+++ sqls/1init.sql
@@ -1,10 +1,9 @@
-CREATE DATABASE  IF NOT EXISTS `neutralinfo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `neutralinfo`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: 192.168.0.76    Database: neutralinfo
 -- ------------------------------------------------------
--- Server version 8.0.20
+-- Server version	8.0.20
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -188,34 +187,3 @@ SET character_set_client = @saved_cs_client;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2020-12-23 11:49:42
-
---
--- Dumping data for table `channel`
---
-
-LOCK TABLES `channel` WRITE;
-/*!40000 ALTER TABLE `channel` DISABLE KEYS */;
-INSERT INTO `channel` VALUES (1,'新聞'),(2,'社群論壇'),(3,'聊天'),(4,'部落格'),(5,'內容農場'),(6,'影音');
-/*!40000 ALTER TABLE `channel` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
---
--- Dumping data for table `position`
---
-
-LOCK TABLES `position` WRITE;
-/*!40000 ALTER TABLE `position` DISABLE KEYS */;
-INSERT INTO `position` VALUES (1,'民進黨'),(2,'國民黨'),(3,'時力'),(4,'親中'),(5,'泛統派');
-/*!40000 ALTER TABLE `position` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `producer`
---
-
-LOCK TABLES `producer` WRITE;
-/*!40000 ALTER TABLE `producer` DISABLE KEYS */;
-INSERT INTO `producer` VALUES ('appledaily.com','蘋果日報','民進黨*0.5|國民黨*0.5|時力*0.2'),('cna.com.tw','中央社','民進黨*0.45|國民黨*0.5|時力*0.4|親中*0.5'),('cnyes.com','鉅亨','民進黨*0.5|國民黨*0.5'),('ctwant.com','王道旺台','民進黨*0.5|國民黨*0.5'),('ettoday.net','ETToday','國民黨*0.6'),('gamer.com.tw','巴哈姆特','民進黨*0.5|國民黨*0.5'),('ltn.com.tw','自由時報','民進黨*0.75|國民黨*0.25|時力*0.6'),('newtalk.tw','新頭殼','民進黨*0.55|國民黨*0.5|華派*0.55'),('udn.com','聯合新聞網','民進黨*0.35|國民黨*0.75|時力*0.4|親中*0.6|泛統派*0.8'),('yahoo.com.tw','YAHOO台灣','國民黨*0.6');
-/*!40000 ALTER TABLE `producer` ENABLE KEYS */;
-UNLOCK TABLES;
