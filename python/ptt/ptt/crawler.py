9b50e1a527860b8d7959bdcdf8a46659b3110e7e
Shuo-Ren, Lin <ntp890517.ee96@g2.nctu.edu.tw>
2022-01-20 23:01:44 +0800

    fix bug, 避免資料庫沒資料的特殊狀況

--- python/ptt/ptt/crawler.py
+++ python/ptt/ptt/crawler.py
@@ -291,11 +291,10 @@ class PttWebCrawler(object):
             # 所需空間快速增加：因此調整成會先判斷該筆貼文是否已存在，
             # 如以存在就採更新同一筆，如無則新增一筆
 
-            # FIXME: 資料庫沒資料時，以下SQL指令會觸發例外
             query_ptt = """ select * from ptt p where p.article_id = %s """
             rs_ptt = conn.execute(query_ptt, str(article_id))
 
-            if rs_ptt:
+            if rs_ptt and rs_ptt.rowcount:
                 for row in rs_ptt:
                     print(
                         f"update count from:{ row['message_count.all']}, to:{message_count['all']}"
